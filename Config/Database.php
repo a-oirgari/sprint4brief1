89859f1c3f92ecb9fa7d288a9883d0fee7b37d29
a-oirgari <abdrahmanghali1998@gmail.com>
2025-12-27 15:25:51 +0100

    PDO connextion to database

---
+++ Config/Database.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Config;
+use \PDO;
+
+class Database
+{
+    private static ?PDO $instance = null;
+
+    public static function getConnection(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = new PDO(
+                "mysql:host=localhost;dbname=hospital_management;charset=utf8",
+                "root",
+                "",
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+        }
+        return self::$instance;
+    }
+}
