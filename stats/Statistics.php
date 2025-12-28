405b9d9dfaa9e0086fdc362b8576f6d5ec58c2e5
a-oirgari <abdrahmanghali1998@gmail.com>
2025-12-28 14:02:12 +0100

    the statistics are implemented

--- stats/Statistics.php
+++ stats/Statistics.php
@@ -1,1 +1,15 @@
-<!-- qdqsdx -->
+<?php
+
+require_once 'config/Database.php';
+
+class Statistics
+{
+    public static function averagePatientAge(): float
+    {
+        $db = Database::getConnection();
+        return (float)$db->query("
+            SELECT AVG(TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()))
+            FROM patients
+        ")->fetchColumn();
+    }
+}
